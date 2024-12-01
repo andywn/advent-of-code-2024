e23b9318d0a987e23c609e4cfc0ea9491c5e42b7
Andrew Norman <andrewnorman@Andrews-MacBook-Pro.local>
2024-12-01 23:39:07 +0000

    Day-1 Advent of Code

---
+++ day-1/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.gradle.internal.declarativedsl.parsing.main
+
+plugins {
+    kotlin("jvm") version "2.1.0"
+    application
+}
+
+group = "uk.andrewnorman"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass = "uk.andrewnorman.MainKt"
+}
+
+tasks.run {
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ day-1/src/main/kotlin/uk/andrewnorman/Main.kt
@@ -0,0 +1,54 @@
+package uk.andrewnorman.uk.andrewnorman
+
+import java.io.File
+import java.util.stream.IntStream
+import java.util.stream.Stream
+import kotlin.io.bufferedReader
+import kotlin.io.readLines
+import kotlin.math.abs
+
+
+fun main() {
+    val idLists = IdListPair()
+    println(idLists.getDiff())
+    println(idLists.getSimilarityScore())
+}
+
+class IdListPair() {
+
+    val intPairParser = Regex("(\\d+)\\s+(\\d+)")
+    val firstColumn: List<Int>
+    val secondColumn: List<Int>
+    val secondColumnValueCount: Map<Int, Int>
+
+    init {
+        val lines = File("src/main/resources/input.txt")?.bufferedReader()?.readLines()
+
+        firstColumn = lines!!.stream()
+            .map{ it -> intPairParser.matchAt(it, 0)!!.groups[1]!!.value.toInt()}
+            .sorted()
+            .toList()
+
+        secondColumn = lines!!.stream()
+            .map{ it -> intPairParser.matchAt(it, 0)!!.groups[2]!!.value.toInt()}
+            .sorted()
+            .toList()
+
+        secondColumnValueCount = secondColumn.groupingBy { it }.eachCount()
+    }
+
+    fun getDiff(): Int {
+        return IntRange(0, firstColumn.size-1)
+            .map { it -> abs(firstColumn.get(it) - secondColumn.get(it)) }
+            .sum()
+    }
+
+    fun getSimilarityScore(): Int {
+        return IntRange(0, firstColumn.size-1)
+            .map{ it -> firstColumn.get(it)}
+            .distinct()
+            .filter{ it -> secondColumnValueCount.contains(it)}
+            .map{ it -> it * secondColumnValueCount.get(it)!!.toInt() }
+            .sum()
+    }
+}
