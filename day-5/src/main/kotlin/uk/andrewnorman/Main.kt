9bb1345cab1ce1a58ffaa3e97abb37b5af88318a
Andrew Norman <andrew.norman@gmail.com>
2024-12-06 12:37:49 +0000

    day 5 challenge 2

--- day-5/src/main/kotlin/uk/andrewnorman/Main.kt
+++ day-5/src/main/kotlin/uk/andrewnorman/Main.kt
@@ -11,6 +11,12 @@ fun main() {
         .map{ PrintOrder(it) }
         .map{ it.checkRules(rules) }
         .toList().sum())
+
+    println(lines.stream()
+        .filter { regex.matches(it) }
+        .map{ PrintOrder(it) }
+        .map{ it.reorderAndCheckRules(rules) }
+        .toList().sum())
 }
 
 class Rules(input: List<String>) {
@@ -33,21 +39,14 @@ class Rules(input: List<String>) {
 
 class PrintOrder(printOrder: String) {
 
-    val order: List<Int>
-    val indexes: Map<Int, Int>
+    // Could do this with LinkedHashMap, but I think that's overkill for this.
+    val order: List<Int> = printOrder.split(",").stream()
+        .map{ it.toInt() }
+        .toList()
 
-    init {
-        order = printOrder.split(",").stream()
-            .map{ it.toInt() }
-            .toList()
-
-        indexes = IntRange(0, order.size-1).
-                associateBy { order.get(it) }
-    }
-
-    fun checkRules(rules: Rules): Int {
+    fun checkRules(rules: Rules, order: List<Int> = this.order) : Int {
         for(pageOrder in 0..order.size-1) {
-            val rulesMatch = rules.rules[order[pageOrder]]?.stream()?.map { it -> checkSingleRule(it, pageOrder)}?.allMatch{ it } != false
+            val rulesMatch = rules.rules[order[pageOrder]]?.stream()?.map { it -> checkSingleRule(it, pageOrder, order)}?.allMatch{ it } != false
             if (!rulesMatch) {
                 return 0
             }
@@ -55,10 +54,52 @@ class PrintOrder(printOrder: String) {
         return order.get((order.size-1)/2)
     }
 
+    /**
+     * Basic method of tackling this.
+     * Loop through until we find the first rule that breaks. Fix that one rule (i.e. move that value as
+     * short a distance as possible). And then start from the start again and check all rules.
+     *
+     * Alternative solution: we could have:
+     * 1. found all rules that are broken.
+     * 2. work out all values that need to change.
+     * 3. work out the LARGEST they need to move by (i.e. which rule pushes them the furthest).
+     * 4. then re-check.
+     */
+    fun reorderAndCheckRules(rules: Rules): Int {
+        if (checkRules(rules) == 0) {
+            val reorderedList = order.toMutableList()
+            // There's got to be a cleaner way of doing it than this...
+            attempts@for (i in 0..100000) {
+                var reOrdered = false
+                index@for (pageOrder in 0..reorderedList.size - 1) {
 
-    fun checkSingleRule(value: Int, after: Int): Boolean {
-        if (indexes.contains(value)) {
-            if (indexes.get(value)!! < after) {
+                    if (rules.rules[reorderedList.get(pageOrder)]?.stream()?.anyMatch { it ->
+                            if (!checkSingleRule(it, pageOrder, reorderedList)) {
+                                reorderedList.remove(it)
+                                reorderedList.add(pageOrder, it)
+                                true
+                            } else {
+                                false
+                            }
+                        } == true) {
+                        reOrdered = true
+                        break@index
+                    }
+                }
+                if (!reOrdered) {
+                    break@attempts
+                }
+            }
+            return checkRules(rules, reorderedList)
+        } else {
+            return 0
+        }
+    }
+
+
+    fun checkSingleRule(value: Int, after: Int, orderedList: List<Int> = this.order): Boolean {
+        if (orderedList.contains(value)) {
+            if (orderedList.indexOf(value)!! < after) {
                 return false
             }
         }
