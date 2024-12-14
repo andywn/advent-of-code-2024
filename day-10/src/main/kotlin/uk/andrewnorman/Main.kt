cbe3789bfaac310ec2391ad2675dca7a6ccc5f08
Andrew Norman <andrew.norman@gmail.com>
2024-12-14 19:17:30 +0000

    Day 10 challenge 2

--- day-10/src/main/kotlin/uk/andrewnorman/Main.kt
+++ day-10/src/main/kotlin/uk/andrewnorman/Main.kt
@@ -16,18 +16,31 @@ fun main() {
 //            "01329801",
 //            "10456732")
 
-    println(TopographicalMap(lines).process())
+    println(TopographicalMap(lines).score())
+    println(TopographicalMap(lines).rating())
 
 }
 
 class TopographicalMap(val lines: List<String>) {
 
-    fun process(): Long {
+    fun score(): Long {
         var sum: Long = 0
         for(y in 0..lines.size-1) {
             for(x in 0..lines.get(0).length-1) {
                 if (lines[y][x] == '0') {
                     sum += countTrailheads(0, Coords(x, y)).size
+                }
+            }
+        }
+        return sum
+    }
+
+    fun rating(): Long {
+        var sum: Long = 0
+        for(y in 0..lines.size-1) {
+            for(x in 0..lines.get(0).length-1) {
+                if (lines[y][x] == '0') {
+                    sum += countRatings(0, Coords(x, y))
                 }
             }
         }
@@ -49,6 +62,23 @@ class TopographicalMap(val lines: List<String>) {
            nines.addAll(countTrailheads(count+1, it.move(coord)))
         }
         return nines
+    }
+
+    fun countRatings(count: Int, coord: Coords): Int {
+        if (coord.x < 0 || coord.y < 0 || coord.x >= lines.get(0).length || coord.y >= lines.size) {
+            return 0
+        }
+        if ((lines[coord.y][coord.x] - '0') != count) {
+            return 0 // Not the right number.
+        }
+        if (count == 9) {
+            return 1
+        }
+        var trails = 0
+        Direction.values().forEach {
+            trails += (countRatings(count+1, it.move(coord)))
+        }
+        return trails
     }
 
 
