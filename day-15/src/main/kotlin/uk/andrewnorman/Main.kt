71bf2e427a4c646b00f3b671d5057ce133c0ffea
Andrew Norman <andrew.norman@gmail.com>
2024-12-20 21:46:11 +0000

    Day 15 Challenge 2

--- day-15/src/main/kotlin/uk/andrewnorman/Main.kt
+++ day-15/src/main/kotlin/uk/andrewnorman/Main.kt
@@ -42,19 +42,19 @@ class Warehouse {
         val whRegex = Regex("[#.O@]+")
         warehouse = lines.filter { whRegex.matches(it) }
             .map {
-                it.toCharArray().map {
+                it.toCharArray().flatMap {
                     when(it) {
-                        ('#') -> WALL
-                        ('O') -> BOX
-                        ('@') -> GUARD
-                        else -> EMPTY
+                        ('#') -> listOf(WALL, WALL)
+                        ('O') -> listOf(BOX_LEFT, BOX_RIGHT)
+                        ('@') -> listOf(GUARD, EMPTY)
+                        else -> listOf(EMPTY, EMPTY)
                     }
                 }.toMutableList()
             }.toMutableList()
 
-        guard = IntRange(0, lines.size-1)
-            .filter { lines.get(it).contains("@") }
-            .map { Coords(lines.get(it).indexOf("@"), it) }
+        guard = IntRange(0, warehouse.size-1)
+            .filter { warehouse.get(it).contains(GUARD) }
+            .map { Coords(warehouse.get(it).indexOf(GUARD), it) }
             .first()
     }
 
@@ -64,41 +64,74 @@ class Warehouse {
             .flatMap { it.toList() }
             .map{ Direction.findDirection(it) }
             .forEach {
-                if (move(EMPTY, guard, it)) {
+                if (checkMove(GUARD, guard, it)) {
+                    move(EMPTY, GUARD, guard, it)
                     guard = it.move(guard)
                 }
             }
     }
 
-    // Item is the item moving into this location.
-    fun move(item: WarehouseItem, location: Coords, direction: Direction): Boolean {
-        if (warehouse[location.y][location.x] == WALL) {
+    fun checkMove(thisItem: WarehouseItem, location: Coords, direction: Direction): Boolean {
+        if (thisItem == WALL) {
             return false
-        } else if (warehouse[location.y][location.x] == EMPTY) {
-            warehouse[location.y][location.x] = item
+        } else if (thisItem == EMPTY) {
             return true
         } else { // it's a box or a guard.
-            var result = move(warehouse[location.y][location.x], direction.move(location), direction)
-            if (result) {
-                warehouse[location.y][location.x] = item
+            val nextLocation = direction.move(location)
+            var nextItem = warehouse[nextLocation.y][nextLocation.x]
+            var result = checkMove(nextItem, nextLocation, direction)
+            if (direction == Direction.DOWN || direction == Direction.UP) {
+                if (nextItem == BOX_LEFT) {
+                    result = result && checkMove(BOX_RIGHT, Direction.RIGHT.move(nextLocation), direction)
+                } else if (nextItem == BOX_RIGHT) {
+                    result = result && checkMove(BOX_LEFT, Direction.LEFT.move(nextLocation), direction)
+                }
             }
             return result
+        }
+    }
+
+    // Item is the item moving into this location.
+    fun move(replacementItem: WarehouseItem, thisItem: WarehouseItem, location: Coords, direction: Direction) {
+        if (thisItem == WALL) {
+            println("TROUBLE!!!")
+        }
+        else if (thisItem == EMPTY) {
+            warehouse[location.y][location.x] = replacementItem
+        } else { // it's a box or a guard.
+            val nextLocation = direction.move(location)
+            val nextItem = warehouse[nextLocation.y][nextLocation.x]
+            move(thisItem, nextItem, nextLocation, direction)
+            if (replacementItem != EMPTY && (direction == Direction.DOWN || direction == Direction.UP)) {
+                if (thisItem == BOX_LEFT) {
+                    move(EMPTY, BOX_RIGHT, Direction.RIGHT.move(location), direction)
+                } else if (thisItem == BOX_RIGHT) {
+                    move(EMPTY, BOX_LEFT, Direction.LEFT.move(location), direction)
+                }
+            }
+            warehouse[location.y][location.x] = replacementItem
         }
     }
 
     fun countGPS(): Long {
         return IntRange(0, warehouse.size-1).flatMap {
             y -> IntRange(0, warehouse[y].size-1).map { x -> Coords(x, y) }
-        }.filter { warehouse[it.y][it.x] == BOX }
+        }.filter { warehouse[it.y][it.x] == BOX_LEFT }
             .map{ (100L * it.y) + it.x }
             .sum()
     }
 
 }
 
-enum class WarehouseItem {
-    WALL,
-    BOX,
-    GUARD,
-    EMPTY
+enum class WarehouseItem(val c: Char) {
+    WALL('#'),
+    BOX('O'),
+    BOX_LEFT('['),
+    BOX_RIGHT(']'),
+    GUARD('@'),
+    EMPTY('.');
+
+    override fun toString(): String {
+        return "${this.c}"
+    }
 }
