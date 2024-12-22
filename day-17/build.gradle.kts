db1cb888817ef7c5ee82876588b7230902978618
Andrew Norman <andrew.norman@gmail.com>
2024-12-22 21:42:37 +0000

    gradle build

---
+++ day-17/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+group = "uk.andrewnorman"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
