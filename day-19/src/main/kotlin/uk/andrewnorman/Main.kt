83c7fa12cd1efd18adfdcc74e63a052af8fd154f
Andrew Norman <andrew.norman@gmail.com>
2024-12-23 23:01:25 +0000

    Day 19 Challenge 2

--- day-19/src/main/kotlin/uk/andrewnorman/Main.kt
+++ day-19/src/main/kotlin/uk/andrewnorman/Main.kt
@@ -1,6 +1,8 @@
 package uk.andrewnorman
 
 import java.io.File
+import java.util.stream.Collectors
+import kotlin.concurrent.getOrSet
 
 fun main() {
     val lines = File("day-19/src/main/resources/input.txt").bufferedReader().readLines()
@@ -18,42 +20,45 @@ fun main() {
 
 
     println(TowelSorted(lines.get(0)).matchTowels(lines))
+    println(TowelSorted(lines.get(0)).matchAllTowels(lines))
 
 }
 
-class TowelSorted(val towelStr:String) {
+class TowelSorted(towelStr:String) {
 
     val towels = towelStr.split(", ").toSet()
     val maxPatternLength = towels.map { it.length }.max()
-    val cache = mutableMapOf<String, Boolean>()
+    val cache = ThreadLocal<MutableMap<String, Long>>()
+
+    fun matchAllTowels(lines: List<String>): Long {
+        val patterns = Regex("^[wubrg]+$")
+        return lines.parallelStream()
+            .filter { patterns.matches(it) }
+            .collect(Collectors.summingLong( { findNextTowel(it).toLong()} ))
+    }
 
     fun matchTowels(lines: List<String>): Long {
         val patterns = Regex("^[wubrg]+$")
         return lines.parallelStream()
             .filter { patterns.matches(it) }
-            .filter { findNextTowel(it) }
+            .filter{ findNextTowel(it) > 0 }
             .count()
-
     }
 
-    fun findNextTowel(design: String): Boolean {
+    fun findNextTowel(design: String): Long {
+        val c = cache.getOrSet { mutableMapOf<String, Long>() }
         if (design.length == 0) {
-            return true
-        } else if (cache.contains(design)) {
-            return cache.getOrDefault(design, false)
+            return 1
+        } else if (c.contains(design)) {
+            return c.getOrDefault(design, 0)
         }
+        var sum = 0L
         for (i in Math. min(design.length, maxPatternLength) downTo 1) {
             if (towels.contains(design.subSequence(0, i))) {
-                if (findNextTowel(design.substring(i))) {
-                    cache.put(design, true)
-                    return true
-                }
+                sum += findNextTowel(design.substring(i))
             }
         }
-        cache.put(design, false)
-        return false
+        c.putIfAbsent(design, sum)
+        return sum
     }
-
-
-
 }
